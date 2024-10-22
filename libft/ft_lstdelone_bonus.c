cd939d99865e59fb937da71aae5cd6af18a2ed66
Alae MEDDAH <alae.meddah42@gmail.com>
2024-10-22 16:34:12 +0200

    bonus

---
+++ libft/ft_lstdelone_bonus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almeddah <almeddah@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 14:36:26 by almeddah          #+#    #+#             */
+/*   Updated: 2024/10/22 15:25:37 by almeddah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (lst && del)
+	{
+		if (lst->content)
+			del(lst->content);
+		free(lst);
+	}
+}
