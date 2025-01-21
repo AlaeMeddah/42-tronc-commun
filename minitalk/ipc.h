4ad933c590a9bb54f71277530bf3f79958f6a9a8
alae <meddahh.alae@gmail.com>
2025-01-21 23:07:52 +0100

    mlx and minitalk

---
+++ minitalk/ipc.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ipc.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almeddah <almeddah@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/21 17:12:24 by almeddah          #+#    #+#             */
+/*   Updated: 2025/01/21 23:07:52 by almeddah         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef IPC_H
+# define IPC_H
+
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+
+#endif
